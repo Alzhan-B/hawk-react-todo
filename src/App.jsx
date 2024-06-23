5e7a6dc1417391a5e8a644c1dd224be36db45b2b
Alzhan-B <alzhan.braliyev@gmail.com>
2024-06-22 23:02:34 -0400

    Lesson_1_2
    Adding TodoForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
+import TodoList from "./TodoList"
+import AddTodoForm from "./AddTodoForm";
 import "./App.css";
-
-const todoList = [
-  {
-    title: "Start task",
-    id: 1,
-  },
-  {
-    title: "Review task",
-    id: 2,
-  },
-  {
-    title: "Complete task",
-    id: 3,
-  },
-];
 
 function App() {
   return (
     <>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map(function (todoItem) {
-          return <li key={todoItem.id}>{todoItem.title}</li>;
-        })}
-      </ul>
+      <AddTodoForm />
+      <TodoList />
     </>
   );
 }
