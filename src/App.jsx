394a0d2ad77dfc6fbbded69aa8b269d72149b2d9
Alzhan-B <alzhan.braliyev@gmail.com>
2024-08-07 21:42:18 -0400

    Add Loading State

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import { useEffect, useState } from "react";
 function App() {
   // const [todoList, setTodoList] = useSemiPersistentState();
   const [todoList, setTodoList] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     new Promise((resolve, reject) => {
@@ -27,11 +28,17 @@ function App() {
       }, 2000);
     }).then((result) => {
       setTodoList(result.data.todoList)
+      setIsLoading(false)
     });
   }, [])
 
-  useEffect(() => {localStorage.setItem('savedTodoList', JSON.stringify(todoList))}, [todoList])
-
+  useEffect(() => {
+    if(!isLoading) {
+      localStorage.setItem('savedTodoList', JSON.stringify(todoList))
+    }
+  }, [todoList, isLoading])
+    
+  
   
   function addTodo (newTodo) {
     setTodoList([...todoList, newTodo])
