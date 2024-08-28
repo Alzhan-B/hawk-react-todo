dc3603e15af5eee4d0fe53ad2879bd98294dbdbb
Alzhan-B <alzhan.braliyev@gmail.com>
2024-08-28 16:48:20 -0400

    Update Lesson_1_9 with DELETE method

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  
+  const BASE_URL = `https://api.airtable.com/v0/${
+      import.meta.env.VITE_AIRTABLE_BASE_ID
+    }/${import.meta.env.VITE_TABLE_NAME}`;
 
   // Reading data from airtable
   const fetchData = async () => {
@@ -15,12 +19,12 @@ function App() {
     options.headers = {
       Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
     };
-    const url = `https://api.airtable.com/v0/${
-      import.meta.env.VITE_AIRTABLE_BASE_ID
-    }/${import.meta.env.VITE_TABLE_NAME}`;
+    // const url = `https://api.airtable.com/v0/${
+    //   import.meta.env.VITE_AIRTABLE_BASE_ID
+    // }/${import.meta.env.VITE_TABLE_NAME}`;
 
     try {
-      const response = await fetch(url, options);
+      const response = await fetch(BASE_URL, options);
 
       console.log(response);
 
@@ -45,7 +49,7 @@ function App() {
     setIsLoading(false);
   };
 
-  // Creting new data in airtable
+  // Creting new data in airtable - POST Method
   const addNewTodo = async (newTodo) => {
     const airtableData = {
       fields: {
@@ -60,25 +64,14 @@ function App() {
         Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
       },
       body: JSON.stringify(airtableData),
-      // body: JSON.stringify({
-      //  records: [
-      //    {
-      //      fields: {
-      //        title: newTodo.title,
-      //      },
-      //    },
-      //  ],
-      // }),
     };
-    const url = `https://api.airtable.com/v0/${
-      import.meta.env.VITE_AIRTABLE_BASE_ID
-    }/${import.meta.env.VITE_TABLE_NAME}`;
+    // const url = `https://api.airtable.com/v0/${
+    //   import.meta.env.VITE_AIRTABLE_BASE_ID
+    // }/${import.meta.env.VITE_TABLE_NAME}`;
 
     try {
-      const response = await fetch(url, options);
-
+      const response = await fetch(BASE_URL, options);
       console.log(response);
-
       if (!response.ok) {
         const message = `Error: ${response.status}`;
         throw new Error(message);
@@ -99,6 +92,46 @@ function App() {
     }
   };
 
+  // Deleteing data from airtable - DELETE Method
+  const removeTodo = async (id) => {
+    const options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
+      },
+    };
+    // const url = `https://api.airtable.com/v0/${
+    //   import.meta.env.VITE_AIRTABLE_BASE_ID
+    // }/${import.meta.env.VITE_TABLE_NAME}/${id}`;
+
+    try {
+      const response = await fetch(`${BASE_URL}/${id}`, options);
+
+      console.log(response);
+
+      if (!response.ok) {
+        const message = `Error: ${response.status}`;
+        throw new Error(message);
+      }
+
+      const data = await response.json();
+      // console.log("returned data: ", data);
+
+      // const updatedTodo = {
+      //   id: data.id,
+      //   title: data.fields.title,
+      //   createdTime: data.createdTime,
+      // };
+
+      setTodoList((prevTodoList) =>
+        prevTodoList.filter((item) => item.id !== data.id)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -113,10 +146,10 @@ function App() {
     addNewTodo(newTodo);
   }
 
-  function removeTodo(id) {
-    const filteredTodo = todoList.filter((todo) => todo.id !== id);
-    setTodoList(filteredTodo);
-  }
+  // function removeTodo(id) {
+  //   const filteredTodo = todoList.filter((todo) => todo.id !== id);
+  //   setTodoList(filteredTodo);
+  // }
 
   return (
     <BrowserRouter>
